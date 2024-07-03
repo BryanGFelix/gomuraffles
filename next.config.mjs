28492e2dcd9aabf0d8f073d8de3274392e6330f7
Bryan Garcia-Felix <bryangarcia-felix@bryans-air.lan>
2024-07-03 01:01:57 -0400

    more cors updates

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ const nextConfig = {
               script-src 'self' 'unsafe-inline';
               style-src 'self' 'unsafe-inline' https://verify.walletconnect.com;
               img-src 'self' https://verify.walletconnect.com data: blob:;
-              connect-src 'self' https://client.warpcast.com/ https://stream.warpcast.com wss://ws.warpcast.com/ https://relay.walletconnect.com/ wss://relay.walletconnect.com/ https://explorer-api.walletconnect.com/ wss://www.walletlink.org/ https://*.cloudflarestream.com https://cloudflare-eth.com https://gomurafflesapi.onrender.com https://sepolia.base.org;
+              connect-src 'self' https://client.warpcast.com/ https://stream.warpcast.com wss://ws.warpcast.com/ https://relay.walletconnect.com/ wss://relay.walletconnect.com/ https://explorer-api.walletconnect.com/ wss://www.walletlink.org/ https://*.cloudflarestream.com https://cloudflare-eth.com https://gomurafflesapi.onrender.com https://sepolia.base.org https://base-sepolia.g.alchemy.com;
               frame-src 'self' https://verify.walletconnect.com;
             `.replace(/\s+/g, ' ').trim()
           },
