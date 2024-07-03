7c29ca2a9eb894983ee210ca1e10cd5ba2b9a280
Bryan Garcia-Felix <bryangarcia-felix@bryans-air.lan>
2024-07-03 01:33:55 -0400

    button to link

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -164,15 +164,15 @@ app.frame('/:id', async(c) => {
       intents: [
         <TextInput placeholder='How many tickets would you like?'/>,
         <Button.Transaction target={`/purchaseTickets/${id}/${ticketPriceFormattedParam}`}>Purchase Tickets</Button.Transaction>,
-        <Button.Redirect location={`gomuraffles.com/raffle/${id}`}>View Details</Button.Redirect>,
+        <Button.Link href={`gomuraffles.com/raffle/${id}`}>View Details</Button.Link>,
       ],
     })
   } else {
     return c.res({
       image: generalImage,
       intents: [
-        <Button.Redirect location={`gomuraffles.com/raffle/${id}`}>View Details</Button.Redirect>,
-        <Button.Redirect location={`gomuraffles.com`}>Create Raffle</Button.Redirect>,
+        <Button.Link href={`gomuraffles.com/raffle/${id}`}>View Details</Button.Link>,
+        <Button.Link href={`gomuraffles.com`}>Create Raffle</Button.Link>,
       ],
     })
   }
